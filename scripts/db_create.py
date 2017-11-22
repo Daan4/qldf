773a316c0462a8a87bce716cff2bcec4f923a5f6
Daan <daan_lubbers@hotmail.com>
2017-11-22 14:02:45 +0100

    Add qldf package to path

--- scripts/db_create.py
+++ scripts/db_create.py
@@ -1,4 +1,8 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
 from qldf import db, create_app
 
-create_app('config.config')
-db.create_all()
+app = create_app('config.config')
+with app.app_context():
+    db.create_all()
