6067e477ab999fca5374bf4bb322526f1852ba7f
Daan <daan_lubbers@hotmail.com>
2017-11-23 15:39:53 +0100

    Add request timing. Add routing prototype functions (unimplemented)

--- qldf/views.py
+++ qldf/views.py
@@ -1,11 +1,60 @@
-from flask import Blueprint, render_template, session, url_for
+from flask import Blueprint, render_template, session, url_for, g, request
+from time import time
 
-bp_root = Blueprint('root', __name__, url_prefix='/')
+root = Blueprint('root', __name__, url_prefix='/')
 
 
-@bp_root.route('/')
+
+@root.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@root.route('/servers')
+def servers():
+    pass
+
+
+@root.route('/players')
+def players():
+    pass
+
+
+@root.route('/records')
+def records():
+    pass
+
+
+@root.route('/maps')
+def maps():
+    pass
+
+
+@root.route('/api')
+def api():
+    pass
+
+
+@root.before_app_request
+def before_request():
+    # Save current time. Used to time request duration.
+    g.start_time = time()
+
+
+@root.after_app_request
+def after_request(response):
+    """ Replace the string __EXECUTION_TIME__ in the response with the execution time."""
+    session['previous_page'] = request.url
+    execution_time = round((time() - g.start_time) * 1000)
+    execution_time_string = '1 millisecond' if execution_time == 1 else f'{execution_time} milliseconds'
+    if response.response:
+        try:
+            response.response[0] = response.response[0].replace('__EXECUTION_TIME__'.encode('utf-8'), execution_time_string.encode('utf-8'))
+            response.headers['content-length'] = len(response.response[0])
+        except TypeError:
+            # Response does not contain the string __EXECUTION_TIME__
+            pass
+    return response
 
 
 def setup_error_routing(app):
