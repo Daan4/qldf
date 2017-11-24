d1c695a2ff5642dbe68a77b46113fd1dd9a134b8
Daan Lubbers <daan_lubbers@hotmail.com>
2017-11-24 22:01:21 +0100

    dont set config vars

--- scripts/deploy_heroku.py
+++ scripts/deploy_heroku.py
@@ -1,13 +1,10 @@
 """
-First time setup of environment variables and postgres addon when deploying on Heroku
+First time setup of postgres db when deploying on Heroku
 Run with
 heroku run python scripts/deploy_heroku.py
 """
-import os
 import subprocess
 
-# Set heroku config variables
-subprocess.call(['heroku', 'config:set', 'QLDF_CONFIG=config.heroku_config', 'SECRET_KEY=ENTERTHESECRETKEYHERESOMEHOW', 'PORT=5000'])
 # Create database with test data
 subprocess.call(['python', 'scripts/db_create.py'])
 subprocess.call(['python', 'scripts/db_populate.py'])
