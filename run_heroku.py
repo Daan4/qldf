aa2ac13d52aa555532a438a93493b1c57831d030
Daan Lubbers <daan_lubbers@hotmail.com>
2017-11-24 20:03:11 +0100

    heroku

--- run_heroku.py
+++ run_heroku.py
@@ -1,8 +1,9 @@
 """Runs the app after being deployed on Heroku."""
 
 from qldf import create_app
+import os
 
 app = create_app('config.heroku_config')
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, port=33507)
+    app.run(debug=True, use_reloader=False, port=int(os.environ.get('PORT', 5000)), host='0.0.0.0')
