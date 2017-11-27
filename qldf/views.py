61a9d60c7e7833e68b9f3b2d6fef0225a81ba5d4
Daan <daan_lubbers@hotmail.com>
2017-11-27 16:30:39 +0100

    Add search form on every page that extends base.html

--- qldf/views.py
+++ qldf/views.py
@@ -4,17 +4,27 @@ from .models import Player, Record, Map, WorkshopItem
 from .forms import SearchForm
 from sqlalchemy import func, desc, asc, literal
 from qldf import db
+from functools import wraps
 
 root = Blueprint('root', __name__, url_prefix='/', template_folder='templates', static_folder='static')
 
 
+def search_form(wrapped_function, *args, **kwargs):
+    """To be used on every routing function that extends base.html to handle the search form in the page header.
+    Requires methods GET and POST enabled"""
+    @wraps(wrapped_function)
+    def wrapper(*args, **kwargs):
+        if g.search_form.validate_on_submit() and g.search_form.search.data:
+            # redirect to search results if search was made
+            return redirect(url_for('root.search', search_string=g.search_form.search_string.data))
+        else:
+            return wrapped_function(*args, **kwargs)
+    return wrapper
+
+
 @root.route('/', methods=['GET', 'POST'])
+@search_form
 def index():
-    # Add search form
-    search_form = SearchForm()
-    if search_form.validate_on_submit() and search_form.search.data:
-        # redirect to search results if search was made
-        return redirect(url_for('root.search', search_string=search_form.search_string.data))
     # Get rows of recent records
     recent_records = db.session.query(Record.mode,
                                       Map.name.label('map_name'),
@@ -56,19 +66,20 @@ def index():
     return render_template('index.html',
                            recent_records=recent_records,
                            recent_world_records=recent_world_records,
-                           recent_maps=recent_maps,
-                           search_form=search_form)
+                           recent_maps=recent_maps)
 
 
-@root.route('servers/')
+@root.route('servers/', methods=['GET', 'POST'])
+@search_form
 def servers():
     return render_template('servers.html')
 
 
-@root.route('player/<string:steam_id>/', defaults={'page': 1, 'sortby': 'date', 'sortdir': 'desc'})
-@root.route('player/<string:steam_id>/<int:page>/<string:sortby>/<string:sortdir>/')
-@root.route('player/<string:steam_id>/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'})
-@root.route('player/<string:steam_id>/<int:page>/', defaults={'sortby': 'date', 'sortdir': 'desc'})
+@root.route('player/<string:steam_id>/', defaults={'page': 1, 'sortby': 'date', 'sortdir': 'desc'}, methods=['GET', 'POST'])
+@root.route('player/<string:steam_id>/<int:page>/<string:sortby>/<string:sortdir>/', methods=['GET', 'POST'])
+@root.route('player/<string:steam_id>/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'}, methods=['GET', 'POST'])
+@root.route('player/<string:steam_id>/<int:page>/', defaults={'sortby': 'date', 'sortdir': 'desc'}, methods=['GET', 'POST'])
+@search_form
 def player(page, steam_id, sortby, sortdir):
     """Show records and stats for a single player"""
     if sortdir == 'asc':
@@ -109,10 +120,11 @@ def player(page, steam_id, sortby, sortdir):
                            reverse_sortdir_on=sortby)
 
 
-@root.route('players/', defaults={'page': 1, 'sortby': 'name', 'sortdir': 'asc'})
-@root.route('players/<int:page>/', defaults={'sortby': 'name', 'sortdir': 'asc'})
-@root.route('players/<int:page>/<string:sortby>/<string:sortdir>/')
-@root.route('players/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'})
+@root.route('players/', defaults={'page': 1, 'sortby': 'name', 'sortdir': 'asc'}, methods=['GET', 'POST'])
+@root.route('players/<int:page>/', defaults={'sortby': 'name', 'sortdir': 'asc'}, methods=['GET', 'POST'])
+@root.route('players/<int:page>/<string:sortby>/<string:sortdir>/', methods=['GET', 'POST'])
+@root.route('players/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'}, methods=['GET', 'POST'])
+@search_form
 def players(page, sortby, sortdir):
     """Show a list of all players and their number of records and world records."""
     if sortdir == 'asc':
@@ -157,10 +169,11 @@ def players(page, sortby, sortdir):
                            reverse_sortdir_on=sortby)
 
 
-@root.route('records/', defaults={'page': 1, 'sortby': 'date', 'sortdir': 'desc'})
-@root.route('records/<int:page>/', defaults={'sortby': 'date', 'sortdir': 'desc'})
-@root.route('records/<int:page>/<string:sortby>/<string:sortdir>/')
-@root.route('records/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'})
+@root.route('records/', defaults={'page': 1, 'sortby': 'date', 'sortdir': 'desc'}, methods=['GET', 'POST'])
+@root.route('records/<int:page>/', defaults={'sortby': 'date', 'sortdir': 'desc'}, methods=['GET', 'POST'])
+@root.route('records/<int:page>/<string:sortby>/<string:sortdir>/', methods=['GET', 'POST'])
+@root.route('records/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'}, methods=['GET', 'POST'])
+@search_form
 def records(page, sortby, sortdir):
     if sortdir == 'asc':
         sortdir = asc
@@ -186,10 +199,11 @@ def records(page, sortby, sortdir):
                            reverse_sortdir_on=sortby)
 
 
-@root.route('map/<string:name>/', defaults={'page': 1, 'sortby': 'rank', 'sortdir': 'asc'})
-@root.route('map/<string:name>/<int:page>/', defaults={'sortby': 'rank', 'sortdir': 'asc'})
-@root.route('map/<string:name>/<int:page>/<string:sortby>/<string:sortdir>/')
-@root.route('map/<string:name>/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'})
+@root.route('map/<string:name>/', defaults={'page': 1, 'sortby': 'rank', 'sortdir': 'asc'}, methods=['GET', 'POST'])
+@root.route('map/<string:name>/<int:page>/', defaults={'sortby': 'rank', 'sortdir': 'asc'}, methods=['GET', 'POST'])
+@root.route('map/<string:name>/<int:page>/<string:sortby>/<string:sortdir>/', methods=['GET', 'POST'])
+@root.route('map/<string:name>/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'}, methods=['GET', 'POST'])
+@search_form
 def _map(page, name, sortby, sortdir):
     """Show records and data for a single map."""
     if sortdir == 'asc':
@@ -235,10 +249,11 @@ def _map(page, name, sortby, sortdir):
                            map_data=map_data)
 
 
-@root.route('maps/', defaults={'page': 1, 'sortby': 'map_name', 'sortdir': 'asc'})
-@root.route('maps/<int:page>/', defaults={'sortby': 'map_name', 'sortdir': 'asc'})
-@root.route('maps/<int:page>/<string:sortby>/<string:sortdir>/')
-@root.route('maps/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'})
+@root.route('maps/', defaults={'page': 1, 'sortby': 'map_name', 'sortdir': 'asc'}, methods=['GET', 'POST'])
+@root.route('maps/<int:page>/', defaults={'sortby': 'map_name', 'sortdir': 'asc'}, methods=['GET', 'POST'])
+@root.route('maps/<int:page>/<string:sortby>/<string:sortdir>/', methods=['GET', 'POST'])
+@root.route('maps/<int:page>/<string:sortby>/', defaults={'sortdir': 'asc'}, methods=['GET', 'POST'])
+@search_form
 def maps(page, sortby, sortdir):
     """Show a list of maps and the number of records on it as well as a workshop url."""
     if sortdir == 'asc':
@@ -261,9 +276,10 @@ def maps(page, sortby, sortdir):
                            reverse_sortdir_on=sortby)
 
 
-@root.route('search/', defaults={'page': 1, 'search_string': ''})
-@root.route('search/<string:search_string>/<int:page>')
-@root.route('search/<string:search_string>/', defaults={'page': 1})
+@root.route('search/', defaults={'page': 1, 'search_string': ''}, methods=['GET', 'POST'])
+@root.route('search/<string:search_string>/<int:page>', methods=['GET', 'POST'])
+@root.route('search/<string:search_string>/', defaults={'page': 1}, methods=['GET', 'POST'])
+@search_form
 def search(page, search_string):
     # subquery with player names
     sq = db.session.query(Player.id.label('id'),
@@ -295,6 +311,9 @@ def api():
 def before_request():
     # Save current time. Used to time request duration.
     g.start_time = time()
+    # Set up the search form for every request
+    # Every page that inherits from base.html has the search form
+    g.search_form = SearchForm()
 
 
 @root.after_app_request
