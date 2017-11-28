d2fa8a05679e6f545154f1cb809d8103d5664867
Daan <daan_lubbers@hotmail.com>
2017-11-28 09:22:57 +0100

    move root forms, models and views into their own folder

--- qldf/__init__.py
+++ qldf/__init__.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_navigation import Navigation
 from .filters import setup_custom_jinja_filters
-import qldf.api
 import os
 
 db = SQLAlchemy()
@@ -19,7 +18,7 @@ def create_app(config, create_logfiles=True):
     # Setup navigation
     setup_navigation(app)
     # Register blueprints
-    from qldf.views import root, setup_error_routing
+    from .root.views import root, setup_error_routing
     from .api.views import api
     app.register_blueprint(api)
     app.register_blueprint(root)
